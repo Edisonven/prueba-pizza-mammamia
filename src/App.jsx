01c0edd46e0379719172eb59422c36eb97ad16c9
Edison <aalevenegass@gmail.com>
2024-02-08 20:42:49 -0300

    Creando componente header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="app__container">
       <Navbar />
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header__container">
+      <h1>Soy un header</h1>
+    </div>
+  );
+};
+
+export default Header;
