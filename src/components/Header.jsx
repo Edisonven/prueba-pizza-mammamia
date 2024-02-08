d0dfaf71be8e4a00fd867817bee4e499ad1ddf9d
Edison <aalevenegass@gmail.com>
2024-02-08 20:49:40 -0300

    Estilando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 const Header = () => {
   return (
     <div className="header__container">
-      <h1>Soy un header</h1>
+      <img className="header__img" src="logo_header.jpg" alt="" />
+      <div className="header__title__section">
+        <h3 className="header__title">¡Pizzería Mamma mia!</h3>
+        <p className="header__paragraph">
+          ¡Las mejores pizzas que podrás encontrar!
+        </p>
+      </div>
     </div>
   );
 };
