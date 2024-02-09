a2b03a7f38d5959c18bfa927531d031cf4add5c1
Edison <aalevenegass@gmail.com>
2024-02-09 17:10:26 -0300

    Separando las pizzas de home a una vista aparte

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,80 +1,10 @@
-import { useContext, useEffect, useState } from "react";
-import { ApiContext } from "../contexts/ContextApi";
-import Button from "react-bootstrap/Button";
-import { useNavigate } from "react-router-dom";
+import Pizzas from "./Pizzas";
 
 const Home = () => {
-  const { apiData, elementFoundById, setElementFoundById } =
-    useContext(ApiContext);
-  const [selectedId, setSelectedId] = useState("");
-
-  const navigate = useNavigate();
-
-  const navigateToDetail = (id) => {
-    const idValueFound = [...apiData].find((pizza) => pizza.id === id);
-
-    setElementFoundById(idValueFound);
-    setSelectedId(idValueFound.id);
-  };
-
-  useEffect(() => {
-    if (selectedId && elementFoundById.id !== undefined) {
-      navigate(`/pizzadetail/${elementFoundById.id}`);
-    }
-  }, [elementFoundById]);
-
-  useEffect(() => {
-    setElementFoundById(null);
-  }, []);
-
   return (
-    <div className="home__container">
+    <div>
       <h4 className="home__title">Echa un vistazo a nuestras pizzas</h4>
-      <div className="home__card__container">
-        {apiData.map((pizza) => (
-          <div key={pizza.id} className="home__card__body">
-            <img className="home__card__img" src={pizza.img} alt="" />
-            <h3 className="home__card__title">
-              {pizza.name.charAt(0).toUpperCase() + pizza.name.slice(1)}
-            </h3>
-            <div className="home__card__ingredients__container">
-              <h5 className="home__card__ingredients__title">Ingredientes:</h5>
-              {pizza.ingredients.map((ingredient, index) => {
-                return (
-                  <ul key={index} className="home__card__ingredients">
-                    <li className="home__card__ingredient">
-                      <img
-                        className="home__card__ingredient__icono"
-                        src="/pizza_icono.png"
-                        alt=""
-                      />
-                      {ingredient.charAt(0).toUpperCase() + ingredient.slice(1)}
-                    </li>
-                  </ul>
-                );
-              })}
-            </div>
-            <hr className="home__Card__line" />
-            <h1 className="home__card__price">$ {pizza.price}</h1>
-            <div className="home__card__details">
-              <Button
-                onClick={() => navigateToDetail(pizza.id)}
-                className="home__card__btn home__card__btn__viewmore"
-                variant="danger"
-                value={pizza.id}
-              >
-                Ver más
-              </Button>
-
-              <div className="home__card__cart">
-                <Button className="home__card__btn " variant="success">
-                  Añadir <img src="/cart.svg" alt="" />
-                </Button>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Pizzas />
     </div>
   );
 };
---
+++ src/views/Pizzas.jsx
@@ -0,0 +1,80 @@
+import { useContext, useEffect, useState } from "react";
+import { ApiContext } from "../contexts/ContextApi";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+const Pizzas = () => {
+  const { apiData, elementFoundById, setElementFoundById } =
+    useContext(ApiContext);
+  const [selectedId, setSelectedId] = useState("");
+
+  const navigate = useNavigate();
+
+  const navigateToDetail = (id) => {
+    const idValueFound = [...apiData].find((pizza) => pizza.id === id);
+
+    setElementFoundById(idValueFound);
+    setSelectedId(idValueFound.id);
+  };
+
+  useEffect(() => {
+    if (selectedId && elementFoundById.id !== undefined) {
+      navigate(`/pizzadetail/${elementFoundById.id}`);
+    }
+  }, [elementFoundById]);
+
+  useEffect(() => {
+    setElementFoundById(null);
+  }, []);
+
+  return (
+    <div className="home__container">
+      <div className="home__card__container">
+        {apiData.map((pizza) => (
+          <div key={pizza.id} className="home__card__body">
+            <img className="home__card__img" src={pizza.img} alt="" />
+            <h3 className="home__card__title">
+              {pizza.name.charAt(0).toUpperCase() + pizza.name.slice(1)}
+            </h3>
+            <div className="home__card__ingredients__container">
+              <h5 className="home__card__ingredients__title">Ingredientes:</h5>
+              {pizza.ingredients.map((ingredient, index) => {
+                return (
+                  <ul key={index} className="home__card__ingredients">
+                    <li className="home__card__ingredient">
+                      <img
+                        className="home__card__ingredient__icono"
+                        src="/pizza_icono.png"
+                        alt=""
+                      />
+                      {ingredient.charAt(0).toUpperCase() + ingredient.slice(1)}
+                    </li>
+                  </ul>
+                );
+              })}
+            </div>
+            <hr className="home__Card__line" />
+            <h1 className="home__card__price">$ {pizza.price}</h1>
+            <div className="home__card__details">
+              <Button
+                onClick={() => navigateToDetail(pizza.id)}
+                className="home__card__btn home__card__btn__viewmore"
+                variant="danger"
+                value={pizza.id}
+              >
+                Ver Detalle
+              </Button>
+
+              <div className="home__card__cart">
+                <Button className="home__card__btn " variant="success">
+                  Añadir <img src="/cart.svg" alt="" />
+                </Button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pizzas;
