94bfb4572206f73c4549bbd46b982f66d09c4cee
Edison <aalevenegass@gmail.com>
2024-02-09 23:29:36 -0300

    Creando algunos estilos para ccomponente carrito

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -1,7 +1,10 @@
 const Carrito = () => {
   return (
-    <div>
-      <h1>Soy el carrito</h1>
+    <div className="carrito__container">
+      <h4 className="carrito__title">Detalles del pedido</h4>
+      <div className="carrito__details__container">
+
+      </div>
     </div>
   );
 };
