0f52938235c9f07635cef8f60fa4a1ec3ba20811
Edison <aalevenegass@gmail.com>
2024-02-14 16:00:08 -0300

    Validando contenido en carrito para mostrar mensaje

--- src/views/Carrito.jsx
+++ src/views/Carrito.jsx
@@ -40,48 +40,58 @@ const Carrito = () => {
   return (
     <div className="carrito__container">
       <h4 className="carrito__title">Detalles del pedido</h4>
-      {pizzasSeleccionadas?.map((pizza) => (
-        <div key={pizza.id} className="carrito__details__container">
-          <div className="carrito__details">
-            <img className="carrito__details__img" src={pizza.img} alt="" />
-            <p className="carrito__details__paragraph">
-              {pizza.name.charAt(0).toUpperCase() + pizza.name.slice(1)}
-            </p>
-          </div>
+      {pizzasSeleccionadas.length < 1 ? (
+        <h3 className="carrito__pedido__title">
+          Aún no tienes nada en el carrito, Selecciona algunas pizzas para
+          comprar
+        </h3>
+      ) : (
+        pizzasSeleccionadas.map((pizza) => (
+          <div key={pizza.id} className="carrito__details__container">
+            <div className="carrito__details">
+              <img className="carrito__details__img" src={pizza.img} alt="" />
+              <p className="carrito__details__paragraph">
+                {pizza.name.charAt(0).toUpperCase() + pizza.name.slice(1)}
+              </p>
+            </div>
 
-          <div className="carrito__price__container">
-            <p className="carrito__price__value">
-              {parseInt(pizza.price * pizza.cantidad).toLocaleString("es-CL", {
-                style: "currency",
-                currency: "CLP",
-              })}
-            </p>
-            <button
-              style={{ backgroundColor: "red" }}
-              onClick={() => {
-                decrement(pizza);
-                disminuirValorPizza(pizza);
-              }}
-              className="carrito__btn btn"
-              variant="danger"
-            >
-              -
-            </button>
-            <p className="carrito__cantidad">{pizza.cantidad}</p>
-            <button
-              //Se llama a la función con el objeto asignado como parámetro
-              onClick={() => {
-                filteredPizzasList(pizza);
-                incrementarValorPizza(pizza);
-              }}
-              className="carrito__btn btn"
-              variant="primary"
-            >
-              +
-            </button>
+            <div className="carrito__price__container">
+              <p className="carrito__price__value">
+                {parseInt(pizza.price * pizza.cantidad).toLocaleString(
+                  "es-CL",
+                  {
+                    style: "currency",
+                    currency: "CLP",
+                  }
+                )}
+              </p>
+              <button
+                style={{ backgroundColor: "red" }}
+                onClick={() => {
+                  decrement(pizza);
+                  disminuirValorPizza(pizza);
+                }}
+                className="carrito__btn btn"
+                variant="danger"
+              >
+                -
+              </button>
+              <p className="carrito__cantidad">{pizza.cantidad}</p>
+              <button
+                //Se llama a la función con el objeto asignado como parámetro
+                onClick={() => {
+                  filteredPizzasList(pizza);
+                  incrementarValorPizza(pizza);
+                }}
+                className="carrito__btn btn"
+                variant="primary"
+              >
+                +
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
       <div className="carrito__total__container">
         <div className="carrito__total__section">
           <h1>Total </h1>
