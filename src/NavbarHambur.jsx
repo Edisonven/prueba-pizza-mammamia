7d738e91250bc29f8a85b948234e84ea26aaede8
Edison <aalevenegass@gmail.com>
2024-02-14 17:35:47 -0300

    Creando menu hamburguesa en modo teléfono

---
+++ src/NavbarHambur.jsx
@@ -0,0 +1,12 @@
+const NavbarHambur = ({ clicked, toggleIcon }) => {
+  return (
+    <div className={`nav-icon3 active ${clicked ? "open" : ""}`} onClick={toggleIcon}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default NavbarHambur;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { CarritoDetailsContext } from "../contexts/ContextCarritoDetails";
+import NavbarHambur from "../NavbarHambur";
 
 const Navbar = () => {
   //Contexto donde se maneja el estado global que actualiza el total a pagar del carrito
   const { totalCarritoValue } = useContext(CarritoDetailsContext);
+  const [clicked, setClicked] = useState(false);
 
+  const toggleIcon = () => {
+    setClicked(!clicked);
+  };
   return (
     <div className="navbar__container">
       <div className="navbar__logo__section">
@@ -14,7 +19,16 @@ const Navbar = () => {
           <h3 className="navbar__title">Mammamía!</h3>
         </Link>
       </div>
-      <div>
+      <div
+        className={`navbar__links__container ${clicked ? "active" : ""}`}
+        onClick={toggleIcon}
+      >
+        <Link className="navbar__carrito__section link__home">
+          Iniciar sesión
+        </Link>
+        <Link className="navbar__carrito__section link__home">Locales</Link>
+        <Link className="navbar__carrito__section link__home">Menu</Link>
+        <Link className="navbar__carrito__section link__home">Promociones</Link>
         <Link to="/carrito" className="navbar__carrito__section link__home">
           Comprar
           <img className="navbar__carrito__img" src="/cart.svg" alt="" />
@@ -26,6 +40,9 @@ const Navbar = () => {
             })}
           </span>
         </Link>
+      </div>
+      <div>
+        <NavbarHambur clicked={clicked} toggleIcon={toggleIcon} />
       </div>
     </div>
   );
