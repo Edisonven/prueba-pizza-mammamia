8fb11373978145f4e7babb3980efbe1e8fe14cb2
Edison <aalevenegass@gmail.com>
2024-02-14 14:31:41 -0300

    Ajustando padding de carrito

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,7 +3,7 @@ import Pizzas from "./Pizzas";
 const Home = () => {
   return (
     <div className="home__container">
-      <h4 className="home__title">Echa un vistazo a nuestras pizzas</h4>
+      <h1 className="home__title">Echa un vistazo a nuestras pizzas</h1>
       <Pizzas />
     </div>
   );
