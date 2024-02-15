4783445159f7f3f7dd5eec205e5b25c374d572e8
Edison <aalevenegass@gmail.com>
2024-02-14 23:15:53 -0300

    Añadiendo íconos a las card de locales

--- src/views/Unicaciones/Locales.jsx
+++ src/views/Unicaciones/Locales.jsx
@@ -14,12 +14,18 @@ const Locales = () => {
             <img className="local__img" src="/location.png" alt="" />
             <h3>{local.city}</h3>
           </div>
-          <p className="local__paragraph local__paragraph__direccion">
-            {local.adress}
-          </p>
-          <p className="local__paragraph local__paragraph__number">
-            Número: <span className="local__number">{local.number}</span>{" "}
-          </p>
+          <div className="local__direction__container">
+            <img src="/map.svg" alt="" />
+            <p className="local__paragraph local__paragraph__direccion">
+              {local.adress}
+            </p>
+          </div>
+          <div className="local__phone__container">
+            <img src="/phone.svg" alt="" />
+            <p className="local__paragraph local__paragraph__number">
+              {local.number}
+            </p>
+          </div>
           <p className="local__paragraph local__paragraph__desc">
             {local.desc}
           </p>
