26f6f8501f06cf50b207ba621e0393c0d979370e
Edison <aalevenegass@gmail.com>
2024-02-15 15:17:19 -0300

    Ajustando promociones modo escritorio

--- src/views/Promociones/Promociones.jsx
+++ src/views/Promociones/Promociones.jsx
@@ -5,29 +5,35 @@ import { PromocionesContext } from "../../contexts/ContextPomociones";
 const Promociones = () => {
   const { promociones } = useContext(PromocionesContext);
   return (
-    <section className="promociones__container">
+    <>
       <h1 className="promociones__title">Nuestras promociones</h1>
-      {promociones?.map((promocion) => (
-        <div key={promocion.id} className="promocion__container">
-          <img className="promociones__img" src={promocion.image} alt="" />
-          <div className="promociones__body">
-            <div className="promociones__title__body">
-              <h4 className="promociones__title__desc">{promocion.title}</h4>
-              <h4 className="promociones__title__desc">{promocion.type}</h4>
-            </div>
-            <div>
-              <p className="promociones__paragraph">{promocion.desc1}</p>
-              <p className="promociones__paragraph">{promocion.desc2}</p>
-              <p className="promociones__paragraph">{promocion.desc3}</p>
-              <p className="promociones__paragraph">{promocion.desc4}</p>
-            </div>
-            <div>
-              <h3 className="promociones__value">Precio: ${promocion.value}</h3>
+      <section className="promociones__container">
+        {promociones?.map((promocion) => (
+          <div key={promocion.id} className="promocion__container">
+            <img className="promociones__img" src={promocion.image} alt="" />
+            <div className="promociones__body">
+              <div className="promociones__title__body">
+                <h4 className="promociones__title__desc">{promocion.title}</h4>
+                <h4 className="promociones__title__desc">{promocion.type}</h4>
+              </div>
+              <div>
+                <p className="promociones__paragraph">{promocion.desc1}</p>
+                <p className="promociones__paragraph">{promocion.desc2}</p>
+                <p className="promociones__paragraph">{promocion.desc3}</p>
+                <p className="promociones__paragraph promociones__paragraph__desc4">
+                  {promocion.desc4}
+                </p>
+              </div>
+              <div>
+                <h3 className="promociones__value">
+                  Precio: ${promocion.value}
+                </h3>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </section>
+        ))}
+      </section>
+    </>
   );
 };
 
